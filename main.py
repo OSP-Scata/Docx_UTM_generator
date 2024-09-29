417956b1b4bb79859b70067f5b286236a45993ce
OSP-Scata <60847538+OSP-Scata@users.noreply.github.com>
2024-09-29 16:32:46 +0400

    Bug fixes

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ FIN = '&utm_content=article'
 
 
 def open_file():
+    ready['text'] = ''
+    selected_file["text"] = ''
     file = filedialog.askopenfile(mode='r', filetypes=[('Документ Word', '*.docx')])
     if file:
         global filepath
@@ -24,21 +26,25 @@ def open_file():
 
 def submit():
     campaign_name = campaign.get()
-    if campaign_name is not None:
+    if not campaign_name:
+        ready['text'] = 'Введите название кампании!'
+    else:
         with open('workfiles/platforms.txt', 'r') as f:
             platforms = f.readlines()
         for platform in platforms:
-            document = Document(filepath)
-            rels = document.part.rels
-            for rel in rels:
-                if rels[rel].reltype == RT.HYPERLINK:
-                    old_url = rels[rel]._target
-                    new_url = old_url + UTM_SOURCE + platform + MEDIUM + campaign_name + FIN
-                    rels[rel]._target = new_url
-            out_file = "workfiles/{name}_{uid}{ext}".format(name=name, uid=platform.rstrip('\n'), ext=ext)
-            document.save(out_file)
-        else:
-            campaign.insert(0, 'Введите название кампании!')
+            try:
+                document = Document(filepath)
+                rels = document.part.rels
+                for rel in rels:
+                    if rels[rel].reltype == RT.HYPERLINK:
+                        old_url = rels[rel]._target
+                        new_url = old_url + UTM_SOURCE + platform + MEDIUM + campaign_name + FIN
+                        rels[rel]._target = new_url
+                out_file = "workfiles/{name}_{uid}{ext}".format(name=name, uid=platform.rstrip('\n'), ext=ext)
+                document.save(out_file)
+                ready['text'] = 'Готово!'
+            except NameError:
+                ready['text'] = 'Укажите файл!'
 
 
 root = tk.Tk()
@@ -61,5 +67,8 @@ campaign.pack(padx=6, pady=3)
 
 submit_button = tk.Button(root, text='Применить', command=submit)
 submit_button.pack(padx=6, pady=10)
+
+ready = tk.Label()
+ready.pack(padx=6, pady=6)
 
 root.mainloop()
