58346d015f1a9e3536d38255b0d28df5d4c46c37
OSP-Scata <60847538+OSP-Scata@users.noreply.github.com>
2024-10-03 18:41:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -29,46 +29,49 @@ def submit():
     if not campaign_name:
         ready['text'] = 'Введите название кампании!'
     else:
-        with open('workfiles/platforms.txt', 'r') as f:
-            platforms = f.readlines()
-        for platform in platforms:
-            try:
-                document = Document(filepath)
-                rels = document.part.rels
-                for rel in rels:
-                    if rels[rel].reltype == RT.HYPERLINK:
-                        old_url = rels[rel]._target
-                        new_url = old_url + UTM_SOURCE + platform + MEDIUM + campaign_name + FIN
-                        rels[rel]._target = new_url
-                out_file = 'workfiles/{name}_{uid}{ext}'.format(name=name, uid=platform.rstrip('\n'), ext=ext)
-                document.save(out_file)
-                ready['text'] = 'Готово!'
-            except NameError:
-                ready['text'] = 'Укажите файл!'
+        try:
+            with open('workfiles/platforms.txt', 'r') as f:
+                platforms = f.readlines()
+            for platform in platforms:
+                try:
+                    document = Document(filepath)
+                    rels = document.part.rels
+                    for rel in rels:
+                        if rels[rel].reltype == RT.HYPERLINK:
+                            old_url = rels[rel]._target
+                            new_url = old_url + UTM_SOURCE + platform + MEDIUM + campaign_name + FIN
+                            rels[rel]._target = new_url
+                    out_file = 'workfiles/{name}_{uid}{ext}'.format(name=name, uid=platform.rstrip('\n'), ext=ext)
+                    document.save(out_file)
+                    ready['text'] = 'Готово!'
+                except NameError:
+                    ready['text'] = 'Укажите файл!'
+        except FileNotFoundError:
+            ready['text'] = 'Отсутствует список площадок! Убедитесь, что в папке workfiles есть файл platforms.txt.'
 
 
 root = tk.Tk()
 root.title('Расстановка UTM-меток в файле')
-root.geometry('400x250')
+root.geometry('400x275')
 
 import_button = tk.Button(root, text='Импорт .docx', command=open_file)
 import_button.pack(padx=6, pady=6)
 
 selected = tk.Label(text='Выбранный файл:')
 selected.pack(padx=3, pady=3)
-selected_file = tk.Label()
+selected_file = tk.Label(root)
 selected_file.pack(padx=6, pady=6)
 
-label = tk.Label(text='Название кампании:')
+label = tk.Label(root, text='Название кампании:')
 label.pack(padx=6, pady=6)
 
-campaign = tk.Entry()
+campaign = tk.Entry(root)
 campaign.pack(padx=6, pady=3)
 
 submit_button = tk.Button(root, text='Применить', command=submit)
 submit_button.pack(padx=6, pady=10)
 
-ready = tk.Label()
+ready = tk.Label(root, wraplength=350)
 ready.pack(padx=6, pady=6)
 
 root.mainloop()
